91f467584b397f41141afe9f3f3088ac7161639c
Daiki Horiyama <d19921207@gmail.com>
2020-09-19 22:15:32 +0900

    twitter v2 APIコールするモジュールの仮実装

---
+++ app/app/Infrastructure/Domain/Tweet/TweetSearcher.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_type=1);
+
+namespace App\Infrastructure\Domain\Tweet;
+
+use App\Domain\Tweet\TweetCollection;
+use App\Domain\Tweet\TweetSearcher as TweetSearcherInterface;
+use App\Domain\Tweet\TweetSearcher\Criteria;
+
+/**
+ * Class TweetSearcher
+ * @package App\Infrastructure\Domain\Tweet
+ */
+class TweetSearcher implements TweetSearcherInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function search(Criteria $criteria): TweetCollection
+    {
+        // stub
+        // TODO: impl
+        return new TweetCollection();
+    }
+}
---
+++ app/app/Infrastructure/Gateway/Twitter/v2/SearchRecentTweetsGateway.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_type=1);
+
+namespace App\Infrastructure\Gateway\Twitter\v2;
+
+use App\Infrastructure\Gateway\Twitter\v2\SearchRecentTweetsGateway\Request;
+use App\Infrastructure\Gateway\Twitter\v2\SearchRecentTweetsGateway\Response;
+
+/**
+ * Class SearchRecentTweetsGateway
+ * @package App\Infrastructure\Gateway\Twitter\v2
+ */
+class SearchRecentTweetsGateway
+{
+    /**
+     * @param Request $request
+     * @return Response
+     * TODO: specify throws
+     */
+    public function call(Request $request): Response
+    {
+        // TODO: impl
+        return new Response();
+    }
+}
