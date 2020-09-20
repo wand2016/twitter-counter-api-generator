ac91e609c60b29442aee53af45940e99e0cebbc8
Daiki Horiyama <d19921207@gmail.com>
2020-09-21 06:07:26 +0900

    検索周りwip

--- app/app/Infrastructure/Domain/Tweet/TweetSearcher.php
+++ app/app/Infrastructure/Domain/Tweet/TweetSearcher.php
@@ -7,6 +7,9 @@ namespace App\Infrastructure\Domain\Tweet;
 use App\Domain\Tweet\TweetCollection;
 use App\Domain\Tweet\TweetSearcher as TweetSearcherInterface;
 use App\Domain\Tweet\TweetSearcher\Criteria;
+use App\Infrastructure\Gateway\Twitter\v2\BearerTokenPool;
+use App\Infrastructure\Gateway\Twitter\v2\SearchRecentTweetsGateway\QueryStringifier;
+use App\Infrastructure\Gateway\Twitter\v2\SearchRecentTweetsGateway\Request;
 
 /**
  * Class TweetSearcher
@@ -15,10 +18,41 @@ use App\Domain\Tweet\TweetSearcher\Criteria;
 class TweetSearcher implements TweetSearcherInterface
 {
     /**
+     * @var BearerTokenPool
+     */
+    private BearerTokenPool $bearerTokenPool;
+
+    /**
+     * @var QueryStringifier
+     */
+    private QueryStringifier $queryStringifier;
+
+    /**
+     * TweetSearcher constructor.
+     * @param BearerTokenPool $bearerTokenPool
+     * @param QueryStringifier $queryStringifier
+     */
+    public function __construct(BearerTokenPool $bearerTokenPool, QueryStringifier $queryStringifier)
+    {
+        $this->bearerTokenPool = $bearerTokenPool;
+        $this->queryStringifier = $queryStringifier;
+    }
+
+
+    /**
      * @inheritDoc
      */
     public function search(Criteria $criteria): TweetCollection
     {
+        $bearerToken = $this->bearerTokenPool->getBearerToken();
+
+        $request = new Request(
+            $this->queryStringifier->stringifyMatch($criteria->getMatch()),
+            null,
+            null,
+            null
+        );
+
         // stub
         // TODO: impl
         return new TweetCollection();
