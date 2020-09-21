a5690909f8f6e943cb4296a1a53123f86c9a3a51
Daiki Horiyama <d19921207@gmail.com>
2020-09-21 18:44:16 +0900

    当座の集計APIのリポジトリモック作成

---
+++ app/app/Infrastructure/Domain/TweetSearchAggregateResultApi/TweetSearchAggregateResultApiRepository.php
@@ -0,0 +1,151 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Domain\TweetSearchAggregateResultApi;
+
+use App\Domain\Tweet\TweetSearcher\Criteria;
+use App\Domain\TweetSearchAggregateResultApi\TweetSearchAggregateResultApi;
+use App\Domain\TweetSearchAggregateResultApi\TweetSearchAggregateResultApi\Id;
+use App\Domain\TweetSearchAggregateResultApi\TweetSearchAggregateResultApiRepository as RepositoryInterface;
+use BadMethodCallException;
+
+/**
+ * Class TweetSearchAggregateResultApiRepository
+ * @package App\Infrastructure\Domain\TweetSearchAggregateResultApi
+ */
+class TweetSearchAggregateResultApiRepository implements RepositoryInterface
+{
+    /**
+     * まだ仮実装のmock
+     * @var TweetSearchAggregateResultApi[]
+     */
+    private array $apis = [];
+
+    /**
+     * TweetSearchAggregateResultApiRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->putSyaroshico();
+        $this->putCocoshico();
+        $this->putRizeshico();
+        $this->putChiyashico();
+        $this->putShicoDiary();
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function findById(Id $id): TweetSearchAggregateResultApi
+    {
+        // TODO: Implement findById() method.
+        throw new BadMethodCallException('not implemented yet');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findAll(): iterable
+    {
+        return $this->apis;
+    }
+
+    protected function putSyaroshico(): void
+    {
+        $id = new Id('11111111-1111-1111-1111-111111111111');
+        $this->apis[$id->getValue()] = new TweetSearchAggregateResultApi(
+            $id,
+            new TweetSearchAggregateResultApi\EndpointName('syaroshico'),
+            new Criteria(
+                new Criteria\Match\LogicalAnd(
+                    new Criteria\Match\LogicalOr(
+                        new Criteria\Match\Keyword('シャロシコ'),
+                        new Criteria\Match\Keyword('syaroshico')
+                    ),
+                    new Criteria\Match\NotRetweet()
+                ),
+                Criteria\Period::unbound()
+            )
+        );
+    }
+
+    protected function putCocoshico(): void
+    {
+        $id = new Id('22222222-2222-2222-2222-222222222222');
+        $this->apis[$id->getValue()] = new TweetSearchAggregateResultApi(
+            $id,
+            new TweetSearchAggregateResultApi\EndpointName('cocoshico'),
+            new Criteria(
+                new Criteria\Match\LogicalAnd(
+                    new Criteria\Match\LogicalOr(
+                        new Criteria\Match\Keyword('ココシコ'),
+                        new Criteria\Match\Keyword('シココア'),
+                        new Criteria\Match\Keyword('shicocoa'),
+                    ),
+                    new Criteria\Match\NotRetweet()
+                ),
+                Criteria\Period::unbound()
+            )
+        );
+    }
+
+    protected function putRizeshico(): void
+    {
+        $id = new Id('33333333-3333-3333-3333-333333333333');
+        $this->apis[$id->getValue()] = new TweetSearchAggregateResultApi(
+            $id,
+            new TweetSearchAggregateResultApi\EndpointName('rizeshico'),
+            new Criteria(
+                new Criteria\Match\LogicalAnd(
+                    new Criteria\Match\LogicalOr(
+                        new Criteria\Match\Keyword('リゼシコ'),
+                        new Criteria\Match\Keyword('rizeshico'),
+                    ),
+                    new Criteria\Match\NotRetweet()
+                ),
+                Criteria\Period::unbound()
+            )
+        );
+    }
+
+    protected function putChiyashico(): void
+    {
+        $id = new Id('44444444-4444-4444-4444-444444444444');
+        $this->apis[$id->getValue()] = new TweetSearchAggregateResultApi(
+            $id,
+            new TweetSearchAggregateResultApi\EndpointName('chiyashico'),
+            new Criteria(
+                new Criteria\Match\LogicalAnd(
+                    new Criteria\Match\LogicalOr(
+                        new Criteria\Match\Keyword('チヤシコ'),
+                        new Criteria\Match\Keyword('千夜シコ'),
+                        new Criteria\Match\Keyword('chiyashico'),
+                    ),
+                    new Criteria\Match\NotRetweet()
+                ),
+                Criteria\Period::unbound()
+            )
+        );
+    }
+
+    protected function putShicoDiary(): void
+    {
+        $id = new Id('99999999-9999-9999-9999-999999999999');
+        $this->apis[$id->getValue()] = new TweetSearchAggregateResultApi(
+            $id,
+            new TweetSearchAggregateResultApi\EndpointName('shico-diary'),
+            new Criteria(
+                new Criteria\Match\LogicalAnd(
+                    new Criteria\Match\LogicalOr(
+                        new Criteria\Match\Account('d_horiyama_core'),
+                        new Criteria\Match\Account('d_horiyama_ota'),
+                    ),
+                    new Criteria\Match\Keyword('#しこにっき'),
+                ),
+                Criteria\Period::unbound()
+            )
+        );
+    }
+}
