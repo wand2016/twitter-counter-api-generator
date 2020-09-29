15dbd85dda12d104fbbfdc6eadfae633aa4da00d
Daiki Horiyama <d19921207@gmail.com>
2020-09-30 00:09:37 +0900

    長すぎ行ignore

--- app/tests/Unit/Infrastructure/Gateway/Twitter/v2/SearchRecentTweetsGateway/Dto/ResponseDtoFactoryTest.php
+++ app/tests/Unit/Infrastructure/Gateway/Twitter/v2/SearchRecentTweetsGateway/Dto/ResponseDtoFactoryTest.php
@@ -33,6 +33,7 @@ class ResponseDtoFactoryTest extends TestCase
     public function testParseResponseBodyContents(): void
     {
         $result = $this->sut->parseResponseBodyContents(
+        // phpcs:ignore Generic.Files.LineLength
             '{"data":[{"created_at":"2020-09-29T15:00:03.000Z","id":"1310957520228429833","text":"2020年9月30日\nごちうさキャラ&amp;中の人カウントダウン\n#青山ブルーマウンテン生誕祭2020 まであと27日\n#水瀬いのり生誕祭2020 まであと63日"}],"meta":{"newest_id":"1310957520228429833","oldest_id":"1310957520228429833","result_count":1}}'
         );
 
